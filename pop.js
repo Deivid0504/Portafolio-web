7e1c85f560009c3cbdf17bee20b122b4b6fb35fb
David_Díaz <diazhoilluisdavid@gmail.com>
2023-08-04 23:33:06 +0200

    css

---
+++ pop.js
@@ -0,0 +1,40 @@
+
+var btnAbrirPopup = document.getElementById('bet2'),
+  overlay = document.getElementById('overlay'),
+      popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+  
+  btnAbrirPopup.addEventListener('click', function(){
+    overlay.classList.add('active');
+    popup.classList.add('active');
+  });
+  
+    btnCerrarPopup.addEventListener('click', function(){
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+  });
+  //acción del botón
+  
+  const bott=document.getElementById('obj3');
+  const capturadt =() => {
+    let nomb,asunto, num;
+    nomb=document.getElementById('obj1').value;
+    asunto=document.getElementById('obj2').value;
+    num=6441116541;
+  
+    var win= window.open(`https://wa.me/${num}?text=Hola%20mi%20nombre%20es%20${nomb}%20me%20pueden%20ayudar%20con%20esto:%20${asunto}`,'_blank');
+  }
+  bott.addEventListener('click', capturadt)
+
+
+
+  const btn=document.getElementById('btn2');
+  const captura =() => {
+    let nomb, num;
+    nomb=document.getElementById('subscr-email').value;
+    num=6441116541;
+  
+    var win= window.open(`https://wa.me/${num}?text=Hola%20${nomb}`,'_blank');
+  }
+  btn.addEventListener('click', captura)
+  
